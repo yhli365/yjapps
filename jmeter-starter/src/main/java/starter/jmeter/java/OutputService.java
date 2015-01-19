1672b9f6bb1ff60f1d6cb1608f3bd3e75a2cad1a
yhli365 <yhli365@126.com>
2015-01-19 13:42:47 +0800

    jmeter test: java request

--- jmeter-starter/src/main/java/starter/jmeter/java/OutputService.java
+++ jmeter-starter/src/main/java/starter/jmeter/java/OutputService.java
@@ -4,6 +4,8 @@ import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 服务为：将输入的两个参数通过IO存入文件
@@ -12,12 +14,14 @@ import java.io.PrintWriter;
  * 
  */
 public class OutputService {
+	private SimpleDateFormat df = new SimpleDateFormat(
+			"[yyyy-MM-dd HH:mm:ss.SSS] ");
 
 	public void output(String filename, int a, int b) throws Exception {
 		PrintWriter out = new PrintWriter(new BufferedWriter(
 				new OutputStreamWriter(new FileOutputStream(filename, true))),
 				false);
-		out.println(a + "+" + b + " = " + (a + b));
+		out.println(df.format(new Date()) + a + "+" + b + " = " + (a + b));
 		out.close();
 	}
 
